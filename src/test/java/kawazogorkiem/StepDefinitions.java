2ad00b99ecb06daade656e8e840db4b68fc9bdb7
krzysztofkepinski <k.kepinski@smatrecruiters.com>
2016-09-16 15:56:42 +0200

    simplify step names, remove explicit parameters from scenario

--- src/test/java/kawazogorkiem/StepDefinitions.java
+++ src/test/java/kawazogorkiem/StepDefinitions.java
@@ -3,30 +3,43 @@ package kawazogorkiem;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import com.smartrecruiters.testing.configurator.Configurator;
 
 
 public class StepDefinitions {
+
+    Configurator configurator = new Configurator(this.getClass());
 
     /*
     * TODO have regexes extracted into some external dependency and reuse them across
     * all BDD testing repos. For example, a regex for system logins or allowed passwords.
     */
-    @Given("^a company named ([\\w\\s]+)$")
-    public void aCompany(String name) throws Throwable {
+//    @Given("^a company named ([\\w\\s]+)$")
+    @Given("a company")
+    public void aCompany() throws Throwable {
     }
 
-    @Given("a user with login ([\\w@\\.]+) and password (\\w+)")
-    public void aUserWithLoginAndPassword(String login, String password) {
+    /*
+        I think we should not use explicit names in steps unless they are relevant for test scenario.
+        In this case company's name, user's login and password are irrelevant. They should not be mentioned
+        explicitly in scenario, but provided by Muchomor underneath.
+     */
+
+//    @Given("a user with login ([\\w@\\.]+) and password (\\w+)")
+    @Given("a user in the company's context")
+    public void aUserWithLoginAndPassword() {
 
     }
 
-    @When("([\\w@\\.]+) logs into the system with password (\\w+)")
-    public void userLogsIntoTheSystemWithPassword(String login, String password) {
+//    @When("([\\w@\\.]+) logs into the system with password (\\w+)")
+    @When("the user logs into the system with her password")
+    public void userLogsIntoTheSystemWithPassword() {
 
     }
 
-    @Then("([\\w@\\.]+) is logged into ([\\w\\s]+)'s account")
-    public void userIsLoggedIntoCompanyAccount(String login, String CompanyName) {
+//    @Then("([\\w@\\.]+) is logged into ([\\w\\s]+)'s account")
+    @Then("she is logged into the company's account")
+    public void userIsLoggedIntoCompanyAccount() {
 
     }
 
--- src/test/resources/kawazogorkiem/user_login.feature
+++ src/test/resources/kawazogorkiem/user_login.feature
@@ -4,7 +4,7 @@ Feature: User Login
   and put them in the context of the company they have been assigned to.
 
   Scenario: Login to existing user account
-    Given a company named Cartoon Network
-    And a user with login susan@susan.sr.com and password 3c5x9cfg
-    When susan@susan.sr.com logs into the system with password 3c5x9cfg
-    Then susan@susan.sr.com is logged into Cartoon Network's account
+    Given a company
+    And a user in the company's context
+    When the user logs into the system with her password
+    Then she is logged into the company's account
